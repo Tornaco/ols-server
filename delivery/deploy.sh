04d8074b6e04fc4fe59b3cfd223df5dc09ae0cc5
ian <ian@ians-iMac.local>
2018-08-19 01:21:15 +0800

    add container rm

--- delivery/deploy.sh
+++ delivery/deploy.sh
@@ -7,6 +7,7 @@ if [ ! -n "$1" ] ;then
     exit -1
 fi
 
+docker rm -f $(docker ps -a | grep team-b | awk '{print $1}') || echo 'Not existed'
 
 docker run --rm -d --name team-b-html -p $1:80 -v $(pwd)/frontend/build/:/usr/share/nginx/html -v $(pwd)/config/nginx.conf:/etc/nginx/conf.d/default.conf nginx /etc/nginx
 docker run --rm -d --name team-b-java -p 6985:5000 -v $(pwd)/build/libs/:/app/build/libs -w /app java:8 /bin/bash -c "java -jar /app/build/libs/ci-example-0.0.1-SNAPSHOT.jar"
