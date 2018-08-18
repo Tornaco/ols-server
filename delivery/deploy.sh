5db1d1f4f3fd2798fe912861aa6842024f477384
tornaco <tornaco@163.com>
2018-08-18 21:54:27 +0800

    Fix Jenkin

--- delivery/deploy.sh
+++ delivery/deploy.sh
@@ -6,5 +6,5 @@ if [ ! -n "$1" ] ;then
     exit -1
 fi
 
-docker rm -f $(docker ps -a | grep greeting-app | awk '{print $1}') || echo 'Not existed'
-docker run -d --name greeting-app -p $1:5000 -v $(pwd)/build/libs/:/app/build/libs -w /app java:8 /bin/bash -c "java -jar /app/build/libs/ci-example-0.0.1-SNAPSHOT.jar"
+docker rm -f $(docker ps -a | grep greeting-app-b | awk '{print $1}') || echo 'Not existed'
+docker run -d --name greeting-app-b -p $1:5000 -v $(pwd)/build/libs/:/app/build/libs -w /app java:8 /bin/bash -c "java -jar /app/build/libs/ci-example-0.0.1-SNAPSHOT.jar"
