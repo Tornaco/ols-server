b19d6c0997b62bff400f1eccb962de64a80197a3
tornaco <tornaco@163.com>
2018-08-18 23:28:23 +0800

    Using hascode as token

--- src/main/java/com/thoughtworks/nho/ols/server/auth/UserAuth.java
+++ src/main/java/com/thoughtworks/nho/ols/server/auth/UserAuth.java
@@ -5,6 +5,6 @@ import com.thoughtworks.nho.ols.server.domain.User;
 public class UserAuth {
 
     public String createTokenForUser(User user) {
-        return user.getId() == null ? null : String.valueOf(user.getUsername());
+        return user.getId() == null ? null : String.valueOf(user.getUsername().hashCode());
     }
 }
