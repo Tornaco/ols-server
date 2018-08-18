740911996bafbde448864b93c8a6b1666f9f21ff
ian <ian@ians-iMac.local>
2018-08-19 01:27:43 +0800

    update ports

--- delivery/deploy.sh
+++ delivery/deploy.sh
@@ -10,4 +10,4 @@ fi
 docker rm -f $(docker ps -a | grep team-b | awk '{print $1}') || echo 'Not existed'
 
 docker run --rm -d --name team-b-html -p $1:80 -v $(pwd)/frontend/build/:/usr/share/nginx/html -v $(pwd)/config/nginx.conf:/etc/nginx/conf.d/default.conf nginx /bin/bash -c "systemctl restart nginx"
-docker run --rm -d --name team-b-java -p 6985:5000 -v $(pwd)/build/libs/:/app/build/libs -w /app java:8 /bin/bash -c "java -jar /app/build/libs/ci-example-0.0.1-SNAPSHOT.jar"
+docker run --rm -d --name team-b-java -p 5005:5000 -v $(pwd)/build/libs/:/app/build/libs -w /app java:8 /bin/bash -c "java -jar /app/build/libs/ci-example-0.0.1-SNAPSHOT.jar"
