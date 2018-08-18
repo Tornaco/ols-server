e2786e1f8d4aeb064f79eb70a6efc73d6ad5c87c
ian <ian@ians-iMac.local>
2018-08-19 02:02:31 +0800

    update nginx onf

--- delivery/deploy.sh
+++ delivery/deploy.sh
@@ -10,8 +10,7 @@ fi
 
 
 docker rm -f $(docker ps -a | grep team-b | awk '{print $1}') || echo 'Not existed'
-echo $(pwd)
-docker images
-docker run --rm -d --name team-b-html -p $1:8080 -v $(pwd)/frontend/build/:/usr/share/nginx/html -v $(pwd)/config/:/etc/nginx/conf.d nginx
+ls
+docker run --rm -d --name team-b-html -p $1:8080 -v $(pwd)/frontend/build/:/usr/share/nginx/html -v $(pwd)/config/nginx.conf:/etc/nginx/conf.d/teamb.conf:ro nginx
 docker run --rm -d --name team-b-java -p 5005:5000 -v $(pwd)/build/libs/:/app/build/libs -w /app java:8 /bin/bash -c "java -jar /app/build/libs/ci-example-0.0.1-SNAPSHOT.jar"
 docker ps -a
