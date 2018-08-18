9b2e7f73e66a3107b44901052f505e07089f399e
tornaco <tornaco@163.com>
2018-08-18 23:19:34 +0800

    Add course api

--- src/main/java/com/thoughtworks/nho/ols/server/controllers/OlsController.java
+++ src/main/java/com/thoughtworks/nho/ols/server/controllers/OlsController.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.nho.ols.server.controllers;
 
 import com.thoughtworks.nho.ols.server.auth.UserAuth;
+import com.thoughtworks.nho.ols.server.domain.Course;
 import com.thoughtworks.nho.ols.server.domain.User;
 import com.thoughtworks.nho.ols.server.repo.UserCenter;
 import org.apache.logging.log4j.util.Strings;
@@ -8,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -15,7 +18,7 @@ import java.util.UUID;
  */
 
 @RestController(value = "/api")
-public class RegisterController {
+public class OlsController {
 
     private UserCenter uc = UserCenter.getInstance();
 
@@ -48,6 +51,40 @@ public class RegisterController {
             return new ResponseEntity<>(new UserAuth().createTokenForUser(user), HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/course")
+    public List<Course> getStudentCourse() {
+        try {
+            /*Class.forName("org.h2.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:h2:~/test", "sa", "");
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM studeng_course order  by DATE desc ");*/
+            List<Course> courseList = new ArrayList<>();
+            for (int i = 1; i <= 5; i++) {
+                Course stuCourse = new Course();
+                stuCourse.title = "python";
+                stuCourse.description = "python是一种解释型、面向对象、动态数据类型的高级程序设计语言。" +
+                        "Python由Guido van Rossum于1989年底发明，第一个公开发行版发行于1991年。" +
+                        "像Perl语言一样, Python 源代码同样遵循 GPL(GNU General Public License)协议。";
+                stuCourse.date = "2018-08-15";
+                courseList.add(stuCourse);
+            }
+            /*while (rs.next())
+            {
+                Course stuCourse = new Course();
+                stuCourse.title=rs.getString("TITLE");
+                stuCourse.description=rs.getString("DESCRIPTION");
+                stuCourse.date=rs.getString("DATE");
+                courseList.add(stuCourse);
+            }*/
+            String a = String.join(",", courseList.toString());
+            //return new ResponseEntity<>(a, HttpStatus.OK);
+            return courseList;
+        } catch (Exception e) {
+            //return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            return null;
         }
     }
 }
---
+++ src/main/java/com/thoughtworks/nho/ols/server/domain/Course.java
@@ -0,0 +1,11 @@
+package com.thoughtworks.nho.ols.server.domain;
+
+import lombok.Data;
+
+@Data
+public class Course {
+    public String title;
+    public String description;
+    public String date;
+
+}
