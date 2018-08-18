5aa79ae67c93c90daa2e93a53f5a1b53a10d06fb
tornaco <tornaco@163.com>
2018-08-18 22:09:29 +0800

    Update docker sh

--- delivery/deploy.sh
+++ delivery/deploy.sh
@@ -6,5 +6,9 @@ if [ ! -n "$1" ] ;then
     exit -1
 fi
 
-docker rm -f $(docker ps -a | grep greeting-app-b | awk '{print $1}') || echo 'Not existed'
-docker run -d --name greeting-app-b -p $1:5000 -v $(pwd)/build/libs/:/app/build/libs -w /app java:8 /bin/bash -c "java -jar /app/build/libs/ci-example-0.0.1-SNAPSHOT.jar"
+app=$(docker ps -a | grep greeting-app | awk '{print $1}')
+if [ -z "app" ] then;
+    docker rm -f $app
+fi
+
+docker run -d --name greeting-app-b -p $1:5000 -v $(pwd)/build/libs/:/app/build/libs -w /app java:8 /bin/bash -c "java -jar /app/build/libs/ols-server-0.0.1-SNAPSHOT.jar"
