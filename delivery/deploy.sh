dfee2995877d2f7d836f672766477e2ad18a5292
ian <ian@ians-iMac.local>
2018-08-19 01:54:15 +0800

    list docker containers

--- delivery/deploy.sh
+++ delivery/deploy.sh
@@ -7,9 +7,10 @@ if [ ! -n "$1" ] ;then
     exit -1
 fi
 
-docker ps -a
+
 
 docker rm -f $(docker ps -a | grep team-b | awk '{print $1}') || echo 'Not existed'
 
-docker run --rm -d --name team-b-html -p $1:8080 -v $(pwd)/frontend/build/:/usr/share/nginx/html -v $(pwd)/config/nginx.conf:/etc/nginx/conf.d/default.conf nginx /bin/bash -c "systemctl restart nginx"
+docker run --rm -d --name team-b-html -p $1:8080 -v $(pwd)/frontend/build/:/usr/share/nginx/html -v $(pwd)/config/:/etc/nginx/conf.d nginx /bin/bash -c "systemctl restart nginx"
 docker run --rm -d --name team-b-java -p 5005:5000 -v $(pwd)/build/libs/:/app/build/libs -w /app java:8 /bin/bash -c "java -jar /app/build/libs/ci-example-0.0.1-SNAPSHOT.jar"
+docker ps -a
