8b9d5c58f79308d99e9ab00e15a07d51a035399b
zhengyang <zhengyang@thoughtworks.com>
2018-08-19 09:27:39 +0800

    verify deploy

--- delivery/deploy.sh
+++ delivery/deploy.sh
@@ -13,3 +13,4 @@ docker rm -f $(docker ps -a | grep team-b | awk '{print $1}') || echo 'Not exist
 
 docker run --rm -d --name team-b-html -p $1:8080 -v $(pwd)/frontend/build/:/usr/share/nginx/html -v $(pwd)/config/nginx.conf:/etc/nginx/conf.d/teamb.conf:ro nginx
 docker run --rm -d --name team-b-java -p 5005:5000 -v $(pwd)/build/libs/:/app/build/libs -w /app java:8 /bin/bash -c "java -jar /app/build/libs/ci-example-0.0.1-SNAPSHOT.jar"
+docker ps -a
