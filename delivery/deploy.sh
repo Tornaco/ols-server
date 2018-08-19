5628db6a39e6b7a7ce737cba739137600646b6e3
zhengyang <zhengyang@thoughtworks.com>
2018-08-19 09:38:34 +0800

    update java docker cmd

--- delivery/deploy.sh
+++ delivery/deploy.sh
@@ -12,5 +12,5 @@ fi
 docker rm -f $(docker ps -a | grep team-b | awk '{print $1}') || echo 'Not existed'
 
 docker run --rm -d --name team-b-html -p $1:8080 -v $(pwd)/frontend/build/:/usr/share/nginx/html -v $(pwd)/config/nginx.conf:/etc/nginx/conf.d/teamb.conf:ro nginx
-docker run --rm -d --name team-b-java -p 5005:5000 -v $(pwd)/build/libs/:/app/build/libs -w /app java:8 /bin/bash -c "java -jar /app/build/libs/ci-example-0.0.1-SNAPSHOT.jar"
+docker run --rm -d --name team-b-java -p 5005:5000 -v $(pwd)/build/libs/:/app/build/libs java:8 java -jar /app/build/libs/ci-example-0.0.1-SNAPSHOT.jar
 docker ps -a
